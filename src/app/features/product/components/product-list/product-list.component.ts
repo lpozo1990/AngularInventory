80a1180b7948290e4ac45e69813008a7a6f999f7
lpozo1990 <lpozo1990@gmail.com>
2023-02-18 21:00:45 -0400

    Finish unique validation

--- src/app/features/product/components/product-list/product-list.component.ts
+++ src/app/features/product/components/product-list/product-list.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, Inject, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { Store, select, createFeatureSelector, createSelector } from '@ngrx/store';
 import { ProductState } from '../../store/reducers/product.reducer';
 import * as ProductActions from '../../store/actions/product.actions';
-import { Observable } from 'rxjs';
+import { map, Observable, of, startWith, switchMap } from 'rxjs';
 import { Product } from '../../store/actions/product.model';
 import { getProducts } from '../../store/selectors/product.selectors';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { v4 as uuidv4 } from 'uuid';
+
+export const uniqueNameValidatorAsync = (products: Product[]) => {
+  return (control: AbstractControl): Observable<ValidationErrors | null> => {
+    const name = control.value.trim().toLowerCase();
+    const duplicateProduct = products.find(product => product.name.trim().toLowerCase() === name);
+    return of(duplicateProduct ? { uniqueNameAsync: true } : null);
+  };
+};
+
 
 @Component({
   selector: 'app-product-list',
@@ -16,6 +26,7 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 })
 export class ProductListComponent implements OnInit {
   @ViewChild('dialogTemplate', { static: true })  dialogTemplate!: TemplateRef<any>;
+  @ViewChild('dialogDeleteTemplate', { static: true })  dialogDeleteTemplate!: TemplateRef<any>;
   private dialogRef!: MatDialogRef<TemplateRef<any>>;
 
   dataSource = new MatTableDataSource<Product>([]);
@@ -27,7 +38,10 @@ export class ProductListComponent implements OnInit {
   constructor(private store: Store<ProductState>, private formBuilder: FormBuilder, public dialog: MatDialog) {
     this.productForm = this.formBuilder.group({
       id:[''],
-      name: ['', [Validators.required, Validators.maxLength(20)]],
+      name: ['', {
+        validators:[Validators.required, Validators.maxLength(20), Validators.pattern(/^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]{1,20}$/)],
+        updateOn: 'blur'
+      },],
       serial_number: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(8)]],
       price: ['', [Validators.required, Validators.min(0), Validators.max(1000)]]
     });
@@ -36,6 +50,9 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
     this.store.dispatch(ProductActions.loadProducts());
     this.products$.subscribe(products => {
+      const nameAsyncValidator = uniqueNameValidatorAsync(products);
+      this.productForm.get('name')?.setAsyncValidators(nameAsyncValidator);
+      this.productForm.get('name')?.updateValueAndValidity();
       this.dataSource.data = products;
     });
   }
@@ -44,7 +61,7 @@ export class ProductListComponent implements OnInit {
     this.productForm.reset();
     this.isNew = true;
     this.showDialog = true;
-    const dialogRef = this.dialog.open(this.dialogTemplate, {
+   this.dialogRef = this.dialog.open(this.dialogTemplate, {
       width: '500px'
     });
   }
@@ -61,21 +78,24 @@ export class ProductListComponent implements OnInit {
   
 
   deleteProduct(product: Product): void {
-    // this.productService.deleteProduct(product.id).subscribe(() => {
-    //   const products = this.products$.getValue();
-    //   const index = products.findIndex(p => p.id === product.id);
-    //   if (index >= 0) {
-    //     products.splice(index, 1);
-    //     this.products$.next(products);
-    //     this.dataSource.data = products;
-    //   }
-    // });
+    this.dialogRef = this.dialog.open(this.dialogDeleteTemplate, {
+      width: '500px',
+      data: { product }
+    });
+
+    this.dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        let {id} = product;
+        this.store.dispatch(ProductActions.deleteProduct({ id }));
+      }
+    });
   }
 
   saveProduct(): void {
     if (this.productForm.valid) {
       const product: Product = this.productForm.value;
       if (this.isNew) {
+        product.id =  uuidv4();
         this.store.dispatch(ProductActions.addProduct({ product }));
       } else {
         this.store.dispatch(ProductActions.updateProduct({ product }));
